cb8b4ad77c85cf3ec437a3c9cc7cf50e9725b518
hussein alzibaidi <huseinalzubaidi@hotmail.com>
2021-03-10 18:26:44 +0200

    G

--- public/jsMain/java.js
+++ public/jsMain/java.js
@@ -1,4 +1,4 @@
-var socket = io('https://procsent-console-husseinalaa.vercel.app')
+var socket = io('https://server.procsent.com/')
 socket.emit('processAr' , 'ok')
 socket.on('processAr-re',e=>{
     alert('ses ! its done and thanks to god !')
