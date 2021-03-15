53757f410ff91ebb4a99e28e060db5e489b5359c
hussein alzibaidi <huseinalzubaidi@hotmail.com>
2021-03-15 16:32:36 +0200

    R

--- wrs_prse/procFront/prp.js
+++ wrs_prse/procFront/prp.js
@@ -1,8 +1,34 @@
 const express = require('express');
 const router = express.Router();
-const proc = [
-    {"hello world" : "fuck me" },{"elon musk" : " mars and elon like maya khalefa and johne sence ! "}
-]
+const buffuJs = {
+    "_dvBn" : [
+        {"exdz" : '[0-9]' , "nam" : ' أرقام '},
+        {"exdz":' ', "nam" : ' مسافات ' },
+        {"exdz":'[\u0600-\u06FF]', "nam" : ' أحرف عربية '},
+        {"exdz":'[a-zA-Z]', "nam" : ' أحرف انجليزية '},
+        {"exdz":'[$-/:-?{-~!"^_`"#@]', "nam" : ' رموز و فواصل ' },
+        {"exdz":false,"nam" : ' معالجة الكلمات العربية '},
+        {"exdz":false,"nam" : ' معالجة الكلمات الانجليزية '},
+        {"exdz":false,"nam" : ' أزالة الاقتباسات '},
+        {"exdz":false,"nam" : ' تحويل الاحرف العربية الى احرف أنجليزية '},
+        {"exdz":false,"nam" : ' تحويل الاحرف الانجليزية الى احرف عربية '}
+    ]
+}
+
+const buffuJsEn = {
+    "_dvBn" : [
+        {"exdz" : '[0-9]' , "nam" : ' أرقام '},
+        {"exdz":' ', "nam" : ' مسافات ' },
+        {"exdz":'[\u0600-\u06FF]', "nam" : ' أحرف عربية '},
+        {"exdz":'[a-zA-Z]', "nam" : ' أحرف انجليزية '},
+        {"exdz":'[$-/:-?{-~!"^_`"#@]', "nam" : ' رموز و فواصل ' },
+        {"exdz":false,"nam" : ' معالجة الكلمات العربية '},
+        {"exdz":false,"nam" : ' معالجة الكلمات الانجليزية '},
+        {"exdz":false,"nam" : ' أزالة الاقتباسات '},
+        {"exdz":false,"nam" : ' تحويل الاحرف العربية الى احرف أنجليزية '},
+        {"exdz":false,"nam" : ' تحويل الاحرف الانجليزية الى احرف عربية '}
+    ]
+}
 
 // Question Sections
 router.get('/procsentArQues', (req, res) => {
@@ -25,27 +51,13 @@ router.get('/procsentEnQues', (req, res) => {
 
 // Process Section
 router.get('/procsentAr', (req, res) => {
-    /*try {
-        if(req.headers.referer === "http://localhost:3000" || req.headers.origin === "http://localhost:3000"){
-            console.log("access once here !")
-            res.send(proc)
-        } else {
-            res.send("false")
-        }
-    } catch(err) {
-        res.send("false")
-    }*/
-    console.log(`access once here ! from ${req.headers.origin} Or ${req.headers.referer}`)
-    res.send(proc)
+    //console.log(`access once here ! from ${req.headers.origin} Or ${req.headers.referer}`)
+    res.send(buffuJs)
 })
 
 router.get('/procsentEn', (req, res) => {
-    if(req.headers.referer === "https://procsent.com/" || req.headers.referer === "https://en.procsent.com/"){
-        res.send(data)
-        res.end();
-    } else {
-        return false
-    }
+    //console.log(`access once here ! from ${req.headers.origin} Or ${req.headers.referer}`)
+    res.send(buffuJsEn)
 })
 // END Two
 
