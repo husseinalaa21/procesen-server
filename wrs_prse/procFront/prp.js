5b41b7eaafb342be387930c98d994e5fc8d2fd08
hussein alzibaidi <huseinalzubaidi@hotmail.com>
2021-03-15 16:42:10 +0200

    D

--- wrs_prse/procFront/prp.js
+++ wrs_prse/procFront/prp.js
@@ -17,16 +17,16 @@ const buffuJs = {
 
 const buffuJsEn = {
     "_dvBn" : [
-        {"exdz" : '[0-9]' , "nam" : ' أرقام '},
-        {"exdz":' ', "nam" : ' مسافات ' },
-        {"exdz":'[\u0600-\u06FF]', "nam" : ' أحرف عربية '},
-        {"exdz":'[a-zA-Z]', "nam" : ' أحرف انجليزية '},
-        {"exdz":'[$-/:-?{-~!"^_`"#@]', "nam" : ' رموز و فواصل ' },
-        {"exdz":false,"nam" : ' معالجة الكلمات العربية '},
-        {"exdz":false,"nam" : ' معالجة الكلمات الانجليزية '},
-        {"exdz":false,"nam" : ' أزالة الاقتباسات '},
-        {"exdz":false,"nam" : ' تحويل الاحرف العربية الى احرف أنجليزية '},
-        {"exdz":false,"nam" : ' تحويل الاحرف الانجليزية الى احرف عربية '}
+        {"exdz" : '[0-9]' , "nam" : ' Numbers '},
+        {"exdz":' ', "nam" : ' Spaces ' },
+        {"exdz":'[\u0600-\u06FF]', "nam" : 'Letters Arabic'},
+        {"exdz":'[a-zA-Z]', "nam" : ' Letters English '},
+        {"exdz":'[$-/:-?{-~!"^_`"#@]', "nam" : ' Breaks and symbols ' },
+        {"exdz":false,"nam" : ' Arabic word processing '},
+        {"exdz":false,"nam" : ' English word processing '},
+        {"exdz":false,"nam" : ' Remove quotes '},
+        {"exdz":false,"nam" : ' Converting Arabic letters into English letters '},
+        {"exdz":false,"nam" : ' Converting English letters into Arabic letters '}
     ]
 }
 
