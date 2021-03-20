3daf63fc6fdf68889c43a9881f3a6632aeb9eacd
hussein alzibaidi <huseinalzubaidi@hotmail.com>
2021-03-20 11:41:17 +0200

    R

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser')
 app.use(cookieParser())
 var cors = require('cors');
 app.use(cors({
-    origin: ['https://procesen.com', 'https://server.procesen.com','https://ar.procesen.com' , 'https://en.procesen.com', 'http://localhost:3000', 'http://127.0.0.1:5500/callUs/callus-ar/index.html']
+    origin: ['https://procesen.com', 'https://server.procesen.com','https://ar.procesen.com' , 'https://en.procesen.com', 'http://localhost:3000', 'https://callus.procesen.com/callus-ar/index.html','https://callus.procesen.com/callus-en/index.html']
 }));
 
 const loginBody = require('./public/divLogin.js')
