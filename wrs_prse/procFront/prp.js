1bba3a83eb68860cf18ad31baf1a09a2e6c356c5
hussein alzibaidi <huseinalzubaidi@hotmail.com>
2021-03-30 00:11:38 +0300

    F

--- wrs_prse/procFront/prp.js
+++ wrs_prse/procFront/prp.js
@@ -8,14 +8,14 @@ const buffuJs = {
         {"exdz":'[a-zA-Z]', "nam" : ' أحرف انجليزية '},
         {"exdz":'[$-/:-?{-~!"^_`"#@]', "nam" : ' رموز و فواصل ' },
         {"exdz":'\n', "nam" : ' سطور ' },
+        {"exdz":'[\[+(?=0|1|2|3|4|5|6|7|8|9|٠|٩|٨|٧|٦|٥|٤|٣|٢|١)\]+]',"nam" : ' أزالة الاقتباسات '},
         {"exdz":false,"nam" : ' معالجة الكلمات العربية ',"ex": false, "eo" : [['مرحبا', 'هاي'], ["العالم","الارض"]]},
         {"exdz":false,"nam" : ' معالجة الكلمات الانجليزية ',"ex":false , "eo":[['hello', 'hi'], ["word","earth"]]},
-        {"exdz":false,"nam" : ' أزالة الاقتباسات ', "ex" : true,"eo":'[1]'},
         {"exdz":false,"nam" : ' تحويل الارقام العربية الى ارقام أنجليزية ',"ex":false, "eo" : [['1','2']]},
         {"exdz":false,"nam" : ' تحويل الارقام الانجليزية الى الارقام عربية ',"ex":false, "eo" : [['1','2']]}
     ]
 }
-
+// if ex is true it's mean's delete element as area
 const buffuJsEn = {
     "_dvBn" : [
         {"exdz" : '[0-9]' , "nam" : ' Numbers '},
@@ -24,9 +24,9 @@ const buffuJsEn = {
         {"exdz":'[a-zA-Z]', "nam" : ' Letters English '},
         {"exdz":'[$-/:-?{-~!"^_`"#@]', "nam" : ' Breaks and symbols ' },
         {"exdz":'\n', "nam" : ' Lins ' },
+        {"exdz":'[\[+(?=0|1|2|3|4|5|6|7|8|9|٠|٩|٨|٧|٦|٥|٤|٣|٢|١)\]+]',"nam" : 'Remove quotes '},
         {"exdz":false,"nam" : ' Arabic word processing ',"ex": false, "eo" : [['مرحبا', 'هاي'], ["العالم","الارض"]]},
         {"exdz":false,"nam" : ' English word processing ',"ex":false , "eo":[['hello', 'hi'], ["word","earth"]]},
-        {"exdz":false,"nam" : ' Remove quotes ', "ex" : true,"eo":'[1]'},
         {"exdz":false,"nam" : ' Converting Arabic letters into English letters ',"ex":false, "eo" : [['1','2']]},
         {"exdz":false,"nam" : ' Converting English letters into Arabic letters ',"ex":false, "eo" : [['1','2']]}
     ]
